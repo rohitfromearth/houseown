496cdc04b94f7ccb9d2d642477aef946845d29c9
rohitcaspl <rohit.c@amrutsoftware.com>
2025-08-04 12:22:36 +0530

    change version React

--- src/screens/Dashboard/HousePlanScreen.js
+++ src/screens/Dashboard/HousePlanScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image, ScrollView } from 'react-native';
+import { View, Text,TextInputBase, StyleSheet, Image, ScrollView } from 'react-native';
 import Header from '../../components/Header';
 
 const HousePlanScreen = () => {
